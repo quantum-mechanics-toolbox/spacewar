a0a86f48fb2e7f03bf7d5e8e147995a18e802eb6
quantum-mechanics-toolbox <quantummechanicstoolbox@gmail.com>
2024-05-15 08:29:18 -0700

    240515

--- adafruit/blinkatest.py
+++ adafruit/blinkatest.py
@@ -3,6 +3,7 @@
 import board
 import digitalio
 import busio
+import time
 
 print("Hello, blinka!")
 
@@ -17,5 +18,45 @@ print("I2C ok!")
 # Try to create an SPI device
 spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
 print("SPI ok!")
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+#import busio
+#import digitalio
+#import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+
+# create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P0)
+chan1 = AnalogIn(mcp, MCP.P1)
+chan2 = AnalogIn(mcp, MCP.P2)
+chan3 = AnalogIn(mcp, MCP.P3)
+chan4 = AnalogIn(mcp, MCP.P4)
+chan5 = AnalogIn(mcp, MCP.P5)
+chan6 = AnalogIn(mcp, MCP.P6)
+chan7 = AnalogIn(mcp, MCP.P7)
+
+while True:
+    print(chan0.value, chan1.value, chan2.value, chan3.value)
+    #    print("Raw ADC Value: ", chan.value, "ADC Voltage: " + str(chan.voltage) + "V")
+#    print("Raw ADC Value: ", chan.value, )
+#    print("ADC Voltage: " + str(chan.voltage) + "V")
+    time.sleep(1)
+
+
+
+
 
 print("done!")
